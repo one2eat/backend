e543af765cc2f42177f874a7818423b710daa25c
Raga Subekti <rfilmriot@gmail.com>
2019-05-12 21:41:23 +0700

    Removed Comment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const config = require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
+const cors = require("cors");
 const Sentry = require("@sentry/node");
 
 /**
@@ -8,38 +9,32 @@ const Sentry = require("@sentry/node");
  */
 const Routes = require("./routes");
 
-/**
- * Initialize Express App
- */
+// Initialize Express App
 const app = express();
 
-/**
- * Bodyparser
- */
 app.use(express.json());
-/**
- * Morgan HTTP Request Logger
- */
 app.use(logger("dev"));
+app.use(cors());
 
+// Initialize and Use Sentry
 Sentry.init({
   dsn: process.env.SENTRY_DSN
 });
+
 app.use(Sentry.Handlers.requestHandler());
 
-/**
- * Routes
- */
-
+// Routes
 app.use("/", Routes.Root);
 app.use("/users", Routes.User);
 app.use("/restaurants", Routes.Restaurant);
 app.use("/recipes", Routes.Recipe);
 
+// Handle Sentry
 app.use(function onError(err, req, res, next) {
   res.statusCode = 500;
   res.end(res.sentry + "\n");
 });
+
 /**
  * Express Listener, Listen to Port 6900, if not specified
  */
