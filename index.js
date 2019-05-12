3415e7f23548ccc42588de23bd35c0d3093bbd2b
Raga Subekti <rfilmriot@gmail.com>
2019-05-12 21:22:23 +0700

    Add Sentry

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const config = require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
+const Sentry = require("@sentry/node");
 
 /**
  * Local Import, usually routes
@@ -21,14 +22,24 @@ app.use(express.json());
  */
 app.use(logger("dev"));
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN
+});
+app.use(Sentry.Handlers.requestHandler());
+
 /**
  * Routes
  */
 
 app.use("/", Routes.Root);
-app.use("/user", Routes.User);
+app.use("/users", Routes.User);
 app.use("/restaurants", Routes.Restaurant);
 app.use("/recipes", Routes.Recipe);
+
+app.use(function onError(err, req, res, next) {
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
 /**
  * Express Listener, Listen to Port 6900, if not specified
  */
