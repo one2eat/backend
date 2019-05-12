5ef4654ac594b1ee3949d9a4982d19b61e7408cc
Raga Subekti <rfilmriot@gmail.com>
2019-05-12 22:30:16 +0700

    Add Create Review on RecipeRoute

--- routes/RecipeRoutes.js
+++ routes/RecipeRoutes.js
@@ -1,12 +1,23 @@
+const { Recipe: Middleware, handleValidatorError } = require("../middleware");
+const { checkCreateReviewRecipe } = Middleware;
+
 const {
   createRecipe,
   getAllRecipes,
-  getOneRecipe
+  getOneRecipe,
+  createRecipeReview
 } = require("../controllers").Recipe;
 const route = require("express").Router();
 
 route.post("/", createRecipe);
 route.get("/", getAllRecipes);
 route.get("/:id", getOneRecipe);
+
+route.post(
+  "/:id/review",
+  checkCreateReviewRecipe,
+  handleValidatorError,
+  createRecipeReview
+);
 
 module.exports = route;
