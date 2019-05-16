b27c1fa16596d314e8ec3776560229b68ba48740
Raga Subekti <rfilmriot@gmail.com>
2019-05-16 14:54:34 +0700

    Add Star to Search

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -9,42 +9,52 @@ const searchRecipesAndRestaurant = async (req, res) => {
     }
 
     const rawQuery = `
-			SELECT
-				Recipes.name,
-				Recipes.id,
-				CONCAT( "recipe" ) AS type 
-			FROM
-				Recipes
-				JOIN RecipeIngredients ON Recipes.id = RecipeIngredients.recipesId 
-			WHERE
-				content LIKE $query
-				OR name LIKE $query
-			GROUP BY
-				id UNION ALL
-			SELECT
-				Restaurants.name,
-				Restaurants.id,
-				CONCAT( "restaurant" ) AS type 
-			FROM
-				RestaurantMenus
-				LEFT JOIN Restaurants ON RestaurantMenus.restaurantId = Restaurants.id 
-			WHERE
-				RestaurantMenus.name LIKE $query
-				OR Restaurants.name LIKE $query 
-			GROUP BY
-				id UNION
-			SELECT
-				Restaurants.name,
-				Restaurants.id,
-				CONCAT( "restaurant" ) AS type 
-			FROM
-				RestaurantMenus
-				RIGHT JOIN Restaurants ON RestaurantMenus.restaurantId = Restaurants.id 
-			WHERE
-				RestaurantMenus.name LIKE $query
-				OR Restaurants.name LIKE $query
-			GROUP BY
-				id
+SELECT
+Recipes.id,
+Recipes.\`name\`,
+SUM(RecipeReviews.stars)/stars.length/COUNT(*) as star,
+CONCAT( "recipe" ) AS type
+FROM
+Recipes
+LEFT JOIN RecipeIngredients
+ON Recipes.id = RecipeIngredients.recipesId 
+LEFT JOIN RecipeSteps
+ON RecipeSteps.recipesId = Recipes.id 
+LEFT JOIN RecipeReviews
+ON RecipeReviews.recipesId = Recipes.id
+LEFT JOIN (
+	SELECT recipesId, COUNT(*) as length FROM RecipeReviews
+) stars ON RecipeReviews.recipesId = Recipes.id
+WHERE
+RecipeIngredients.content LIKE $query OR Recipes.name LIKE $query OR RecipeSteps.content LIKE $query
+GROUP BY
+id 
+-- END OF RECIPES
+-- UNION START
+UNION ALL
+-- START OF RESTAURANT
+SELECT
+Restaurants.id,
+Restaurants.\`name\`,
+SUM(RestaurantReviews.stars)/stars.length/COUNT(*) AS star,
+CONCAT("restaurant") as type
+FROM
+RestaurantTags
+LEFT JOIN Restaurants
+ON RestaurantTags.restaurantId = Restaurants.id 
+LEFT JOIN Tags
+ON Tags.id = RestaurantTags.tagId 
+LEFT JOIN RestaurantMenus
+ON RestaurantMenus.restaurantId = Restaurants.id
+LEFT JOIN RestaurantReviews ON RestaurantReviews.restaurantId = Restaurants.id
+LEFT JOIN (
+	SELECT restaurantId, COUNT(*) as length FROM RestaurantReviews
+) stars ON RestaurantReviews.restaurantId = Restaurants.id
+WHERE Restaurants.name LIKE $query OR RestaurantMenus.name LIKE $query OR Tags.name LIKE $query
+GROUP BY
+id
+ORDER BY
+star DESC
     `;
 
     const query = await sequelize.query(rawQuery, {
